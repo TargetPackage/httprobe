203f5f9fda344f66164fb4a024a0377213cab2e7
Aaron Meese <aaron@meese.dev>
2024-02-22 15:13:35 -0500

    Feature additions Includes proxy support and custom user agents (https://github.com/c2biz/httprobe), updates go version (https://github.com/o3oskye/httprobe), andadditional ports (https://github.com/Abdulrahman-Kamel/httprobe).

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"sync"
@@ -34,7 +35,7 @@ func main() {
 
 	// probe flags
 	var probes probeArgs
-	flag.Var(&probes, "p", "add additional probe (proto:port)")
+	flag.Var(&probes, "p", "add additional probe (e.g. -p proto:port or -p <small|large|xlarge>)")
 
 	// skip default probes flag
 	var skipDefault bool
@@ -52,6 +53,14 @@ func main() {
 	var method string
 	flag.StringVar(&method, "method", "GET", "HTTP method to use")
 
+	// HTTP User-Agent to use
+	var userAgent string
+	flag.StringVar(&userAgent, "A", "httprobe", "HTTP User-Agent to use")
+
+	// http/socks proxy
+	var proxyURI string
+	flag.StringVar(&proxyURI, "x", "", "HTTP/SOCKS proxy to use")
+
 	flag.Parse()
 
 	// make an actual time.Duration out of the timeout
@@ -66,6 +75,13 @@ func main() {
 			Timeout:   timeout,
 			KeepAlive: time.Second,
 		}).DialContext,
+	}
+
+	if proxyURI != "" {
+		PROXY_ADDR := proxyURI
+		url_i := url.URL{}
+		url_proxy, _ := url_i.Parse(PROXY_ADDR)
+		tr.Proxy = http.ProxyURL(url_proxy)
 	}
 
 	re := func(req *http.Request, via []*http.Request) error {
@@ -96,7 +112,7 @@ func main() {
 
 				// always try HTTPS first
 				withProto := "https://" + url
-				if isListening(client, withProto, method) {
+				if isListening(client, withProto, method, userAgent) {
 					output <- withProto
 
 					// skip trying HTTP if --prefer-https is set
@@ -120,7 +136,7 @@ func main() {
 		go func() {
 			for url := range httpURLs {
 				withProto := "http://" + url
-				if isListening(client, withProto, method) {
+				if isListening(client, withProto, method, userAgent) {
 					output <- withProto
 					continue
 				}
@@ -163,18 +179,28 @@ func main() {
 		}
 
 		// Adding port templates
+		xxlarge := []string{"8401", "5080", "593", "7443", "4443", "5357", "81", "300", "591", "593", "832", "981", "1010", "1311", "2082", "2087", "2095", "2096", "2480", "3000", "3128", "3333", "4243", "4567", "4711", "4712", "4993", "5000", "5104", "5108", "5800", "6543", "7000", "7396", "7474", "8000", "8001", "8008", "8014", "8042", "8069", "8080", "8081", "8088", "8090", "8091", "8118", "8123", "8172", "8222", "8243", "8280", "8281", "8333", "8443", "8500", "8834", "8880", "8888", "8983", "9000", "9043", "9060", "9080", "9090", "9091", "9200", "9443", "9800", "9981", "12443", "16080", "18091", "18092", "20720", "28017", "280", "591", "777", "900", "2301", "2381", "2688", "2693", "2851", "3106", "3128", "5490", "5554", "6842", "8001", "8002", "8007", "8008", "8010", "8081", "8765", "311", "598", "620", "765", "901", "1085", "1188", "1559", "1739", "1772", "1963", "2314", "2339", "2403", "2534", "2784", "2837", "2907", "2929", "2930", "2972", "3011", "3012", "3334", "3342", "4801", "5987", "6588", "7001", "7002", "7015", "8208", "8383", "8893", "9090", "9280", "10000", "21845", "22555", "25867", "8083"}
 		xlarge := []string{"81", "300", "591", "593", "832", "981", "1010", "1311", "2082", "2087", "2095", "2096", "2480", "3000", "3128", "3333", "4243", "4567", "4711", "4712", "4993", "5000", "5104", "5108", "5800", "6543", "7000", "7396", "7474", "8000", "8001", "8008", "8014", "8042", "8069", "8080", "8081", "8088", "8090", "8091", "8118", "8123", "8172", "8222", "8243", "8280", "8281", "8333", "8443", "8500", "8834", "8880", "8888", "8983", "9000", "9043", "9060", "9080", "9090", "9091", "9200", "9443", "9800", "9981", "12443", "16080", "18091", "18092", "20720", "28017"}
 		large := []string{"81", "591", "2082", "2087", "2095", "2096", "3000", "8000", "8001", "8008", "8080", "8083", "8443", "8834", "8888"}
+		small := []string{"7000", "7001", "8000", "8001", "8008", "8080", "8083", "8443", "8834", "8888", "10000"}
 
 		// submit any additional proto:port probes
 		for _, p := range probes {
 			switch p {
+			case "xxlarge":
+				for _, port := range xxlarge {
+					httpsURLs <- fmt.Sprintf("%s:%s", domain, port)
+				}
 			case "xlarge":
 				for _, port := range xlarge {
 					httpsURLs <- fmt.Sprintf("%s:%s", domain, port)
 				}
 			case "large":
 				for _, port := range large {
+					httpsURLs <- fmt.Sprintf("%s:%s", domain, port)
+				}
+			case "small":
+				for _, port := range small {
 					httpsURLs <- fmt.Sprintf("%s:%s", domain, port)
 				}
 			default:
@@ -210,13 +236,14 @@ func main() {
 	outputWG.Wait()
 }
 
-func isListening(client *http.Client, url, method string) bool {
+func isListening(client *http.Client, url, method string, userAgent string) bool {
 
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
 		return false
 	}
 
+	req.Header.Add("User-Agent", userAgent)
 	req.Header.Add("Connection", "close")
 	req.Close = true
 
