2723b7eaf8d80f79bd8077ebd17ea67bf183e189
Aaron Meese <aaron@meese.dev>
2024-02-22 15:44:06 -0500

    Improved `-p` formatting From https://github.com/NULLHE4D/httprobe.

--- main.go
+++ main.go
@@ -15,15 +15,78 @@ import (
 	"time"
 )
 
-type probeArgs []string
+type probeArgs struct {
+	httpPorts  []string
+	httpsPorts []string
+}
 
 func (p *probeArgs) Set(val string) error {
-	*p = append(*p, val)
+	// Adding port templates
+	xxlarge := []string{"8401", "5080", "593", "7443", "4443", "5357", "81", "300", "591", "593", "832", "981", "1010", "1311", "2082", "2087", "2095", "2096", "2480", "3000", "3128", "3333", "4243", "4567", "4711", "4712", "4993", "5000", "5104", "5108", "5800", "6543", "7000", "7396", "7474", "8000", "8001", "8008", "8014", "8042", "8069", "8080", "8081", "8088", "8090", "8091", "8118", "8123", "8172", "8222", "8243", "8280", "8281", "8333", "8443", "8500", "8834", "8880", "8888", "8983", "9000", "9043", "9060", "9080", "9090", "9091", "9200", "9443", "9800", "9981", "12443", "16080", "18091", "18092", "20720", "28017", "280", "591", "777", "900", "2301", "2381", "2688", "2693", "2851", "3106", "3128", "5490", "5554", "6842", "8001", "8002", "8007", "8008", "8010", "8081", "8765", "311", "598", "620", "765", "901", "1085", "1188", "1559", "1739", "1772", "1963", "2314", "2339", "2403", "2534", "2784", "2837", "2907", "2929", "2930", "2972", "3011", "3012", "3334", "3342", "4801", "5987", "6588", "7001", "7002", "7015", "8208", "8383", "8893", "9090", "9280", "10000", "21845", "22555", "25867", "8083"}
+	xlarge := []string{"81", "300", "591", "593", "832", "981", "1010", "1311", "2082", "2087", "2095", "2096", "2480", "3000", "3128", "3333", "4243", "4567", "4711", "4712", "4993", "5000", "5104", "5108", "5800", "6543", "7000", "7396", "7474", "8000", "8001", "8008", "8014", "8042", "8069", "8080", "8081", "8088", "8090", "8091", "8118", "8123", "8172", "8222", "8243", "8280", "8281", "8333", "8443", "8500", "8834", "8880", "8888", "8983", "9000", "9043", "9060", "9080", "9090", "9091", "9200", "9443", "9800", "9981", "12443", "16080", "18091", "18092", "20720", "28017"}
+	large := []string{"81", "591", "2082", "2087", "2095", "2096", "3000", "8000", "8001", "8008", "8080", "8083", "8443", "8834", "8888"}
+	small := []string{"7000", "7001", "8000", "8001", "8008", "8080", "8083", "8443", "8834", "8888", "10000"}
+
+	pair := strings.SplitN(val, ":", 2)
+	if len(pair) == 1 {
+		// using `-p p1,p2...pn` format
+		fields := strings.Split(pair[0], ",")
+		for _, f := range fields {
+			if f == "small" {
+				p.httpPorts = append(p.httpPorts, small...)
+				p.httpsPorts = append(p.httpsPorts, small...)
+			} else if f == "large" {
+				p.httpPorts = append(p.httpPorts, large...)
+				p.httpsPorts = append(p.httpsPorts, large...)
+			} else if f == "xlarge" {
+				p.httpPorts = append(p.httpPorts, xlarge...)
+				p.httpsPorts = append(p.httpsPorts, xlarge...)
+			} else if f == "xxlarge" {
+				p.httpPorts = append(p.httpPorts, xxlarge...)
+				p.httpsPorts = append(p.httpsPorts, xxlarge...)
+			} else {
+				p.httpPorts = append(p.httpPorts, f)
+				p.httpsPorts = append(p.httpsPorts, f)
+			}
+		}
+	} else if len(pair) == 2 {
+		// using `-p proto:p1,p2...pn` format
+		proto := pair[0]
+		fields := strings.Split(pair[1], ",")
+		for _, f := range fields {
+			if proto == "https" {
+				if f == "small" {
+					p.httpsPorts = append(p.httpsPorts, small...)
+				} else if f == "large" {
+					p.httpsPorts = append(p.httpsPorts, large...)
+				} else if f == "xlarge" {
+					p.httpsPorts = append(p.httpsPorts, xlarge...)
+				} else if f == "xxlarge" {
+					p.httpsPorts = append(p.httpsPorts, xxlarge...)
+				} else {
+					p.httpsPorts = append(p.httpsPorts, f)
+				}
+			} else {
+				if f == "small" {
+					p.httpPorts = append(p.httpPorts, small...)
+				} else if f == "large" {
+					p.httpPorts = append(p.httpPorts, large...)
+				} else if f == "xlarge" {
+					p.httpPorts = append(p.httpPorts, xlarge...)
+				} else if f == "xxlarge" {
+					p.httpPorts = append(p.httpPorts, xxlarge...)
+				} else {
+					p.httpPorts = append(p.httpPorts, f)
+				}
+			}
+		}
+	}
+
 	return nil
 }
 
 func (p probeArgs) String() string {
-	return strings.Join(p, ",")
+	return strings.Join(append(p.httpPorts, p.httpsPorts...), ",")
 }
 
 func main() {
@@ -187,47 +250,16 @@ func main() {
 			httpsURLs <- domain
 		}
 
-		// Adding port templates
-		xxlarge := []string{"8401", "5080", "593", "7443", "4443", "5357", "81", "300", "591", "593", "832", "981", "1010", "1311", "2082", "2087", "2095", "2096", "2480", "3000", "3128", "3333", "4243", "4567", "4711", "4712", "4993", "5000", "5104", "5108", "5800", "6543", "7000", "7396", "7474", "8000", "8001", "8008", "8014", "8042", "8069", "8080", "8081", "8088", "8090", "8091", "8118", "8123", "8172", "8222", "8243", "8280", "8281", "8333", "8443", "8500", "8834", "8880", "8888", "8983", "9000", "9043", "9060", "9080", "9090", "9091", "9200", "9443", "9800", "9981", "12443", "16080", "18091", "18092", "20720", "28017", "280", "591", "777", "900", "2301", "2381", "2688", "2693", "2851", "3106", "3128", "5490", "5554", "6842", "8001", "8002", "8007", "8008", "8010", "8081", "8765", "311", "598", "620", "765", "901", "1085", "1188", "1559", "1739", "1772", "1963", "2314", "2339", "2403", "2534", "2784", "2837", "2907", "2929", "2930", "2972", "3011", "3012", "3334", "3342", "4801", "5987", "6588", "7001", "7002", "7015", "8208", "8383", "8893", "9090", "9280", "10000", "21845", "22555", "25867", "8083"}
-		xlarge := []string{"81", "300", "591", "593", "832", "981", "1010", "1311", "2082", "2087", "2095", "2096", "2480", "3000", "3128", "3333", "4243", "4567", "4711", "4712", "4993", "5000", "5104", "5108", "5800", "6543", "7000", "7396", "7474", "8000", "8001", "8008", "8014", "8042", "8069", "8080", "8081", "8088", "8090", "8091", "8118", "8123", "8172", "8222", "8243", "8280", "8281", "8333", "8443", "8500", "8834", "8880", "8888", "8983", "9000", "9043", "9060", "9080", "9090", "9091", "9200", "9443", "9800", "9981", "12443", "16080", "18091", "18092", "20720", "28017"}
-		large := []string{"81", "591", "2082", "2087", "2095", "2096", "3000", "8000", "8001", "8008", "8080", "8083", "8443", "8834", "8888"}
-		small := []string{"7000", "7001", "8000", "8001", "8008", "8080", "8083", "8443", "8834", "8888", "10000"}
-
 		// submit any additional proto:port probes
-		for _, p := range probes {
-			switch p {
-			case "xxlarge":
-				for _, port := range xxlarge {
-					httpsURLs <- fmt.Sprintf("%s:%s", domain, port)
-				}
-			case "xlarge":
-				for _, port := range xlarge {
-					httpsURLs <- fmt.Sprintf("%s:%s", domain, port)
-				}
-			case "large":
-				for _, port := range large {
-					httpsURLs <- fmt.Sprintf("%s:%s", domain, port)
-				}
-			case "small":
-				for _, port := range small {
-					httpsURLs <- fmt.Sprintf("%s:%s", domain, port)
-				}
-			default:
-				pair := strings.SplitN(p, ":", 2)
-				if len(pair) != 2 {
-					continue
-				}
-
-				// This is a little bit funny as "https" will imply an
-				// http check as well unless the --prefer-https flag is
-				// set. On balance I don't think that's *such* a bad thing
-				// but it is maybe a little unexpected.
-				if strings.ToLower(pair[0]) == "https" {
-					httpsURLs <- fmt.Sprintf("%s:%s", domain, pair[1])
-				} else {
-					httpURLs <- fmt.Sprintf("%s:%s", domain, pair[1])
-				}
-			}
+		for _, p := range probes.httpsPorts {
+			// This is a little bit funny as "https" will imply an
+			// http check as well unless the --prefer-https flag is
+			// set. On balance I don't think that's *such* a bad thing
+			// but it is maybe a little unexpected.
+			httpsURLs <- fmt.Sprintf("%s:%s", domain, p)
+		}
+		for _, p := range probes.httpPorts {
+			httpURLs <- fmt.Sprintf("%s:%s", domain, p)
 		}
 	}
 
